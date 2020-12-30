786a02cf8cc9be0f0267bbb467fd1ad5695ecc1c
Alex Maxwell <66850447+AlexMaxwell2001@users.noreply.github.com>
2020-12-30 22:01:03 +0000

    Add files via upload

---
+++ src/CS335_Lab_week9/ShapeStorage.java
@@ -0,0 +1,17 @@
+package CS335_Lab_week9;
+
+
+public class ShapeStorage {
+	
+	private Shape []shapes = new Shape[5];
+	private int index;
+	
+	public void addShape(String name){
+		int i = index++;
+		shapes[i] = new Shape(i,name);
+	}
+	
+	public Shape[] getShapes(){
+		return shapes;
+	}
+}
